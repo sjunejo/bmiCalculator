08cc09393798d05120a4d06cfd0b8e39b32bba9c
sjunejo <sjunejo1@gmail.com>
2014-11-09 19:45:19 +0000

    Fix unit-test bug

--- bmiCalculator_test.py
+++ bmiCalculator_test.py
@@ -4,15 +4,12 @@ import unittest
 
 class BMICalculatorTest(unittest.TestCase):
     
-    known_values = (("1.8m", "65.6kg", 20.20))
-    
-    def setUp(self):
-        return
+    known_bmi_values = (("1.8m", "65.6kg", 20.20),("5'9", "159lb", 23.5))
     
     def test_calculate_bmi(self):
-        for height, weight, actual_bmi in self.known_values:
+        for (height, weight, actual_bmi) in self.known_bmi_values:
             bmi = bmiCalculator.calculate_bmi(height, weight)
-            self.assertAlmostEqual(bmi, actual_bmi)
+            self.assertAlmostEqual(bmi, actual_bmi, 1)
         
         
         
